80e67aa70f25b0eb124cb7e4f87c5edc9fad3940
dnbh <dnb@cs.jhu.edu>
2018-10-26 09:31:27 -0400

    whitespace

--- src/bindash.cpp
+++ src/bindash.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright [2018] [XiaoFei Zhao]
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -79,11 +79,11 @@ bool ispowof2(T x) { return 0 == (x & (x-1)); }
 
 void entity_init(Entity &entity, CBuf &cbuf, const std::string &entityname, const Sketchargs &args, const size_t nseqs = SIZE_MAX) {
 	entity.name = entityname;
-	uint64_t genome_size = 0;	
+	uint64_t genome_size = 0;
 	std::minstd_rand0 g1(args.randseed);
 	auto s1 = g1();
 	auto s2 = g1();
-		
+
 	if (-1 == args.minhashtype) { // special perfect hash function for nucleotides
 		std::set<uint64_t, std::greater<uint64_t>> signset;
 		_DnaPerfectHash hf(args.kmerlen);
@@ -96,6 +96,7 @@ void entity_init(Entity &entity, CBuf &cbuf, const std::string &entityname, cons
 		genome_size = signset.size();
 		entity.usigs = std::vector<uint64_t>(signset.begin(), signset.end());
 	} else if (0 == args.minhashtype) { // multi minvals one hash function
+        std::fprintf(stderr, "sketchsize64: %zu\n", size_t(args.sketchsize64));
 		std::priority_queue<uint64_t> signqueue;
 		std::set<uint64_t> signset;
 		CyclicHash<uint64_t> hf(args.kmerlen, s1, s2, 64);
@@ -113,7 +114,7 @@ void entity_init(Entity &entity, CBuf &cbuf, const std::string &entityname, cons
 			genome_size = estimate_genome_size0(signqueue.top(), signqueue.size());
 		}
 		size_t inisize = signqueue.size();
-		entity.usigs = std::vector<uint64_t>(inisize, 0);		
+		entity.usigs = std::vector<uint64_t>(inisize, 0);
 		for (size_t i = 0; i < inisize; i++) {
 			entity.usigs[i] = signqueue.top();
 			// std::cerr << "File" << entity.name << " stored value " << entity.usigs[i] << std::endl;
@@ -139,7 +140,7 @@ void entity_init(Entity &entity, CBuf &cbuf, const std::string &entityname, cons
 			if (cbuf.ceof()) { break; }
 			hashupdate1(cbuf, signs, hfs, hfrcs, args.isstrandpreserved, args.sketchsize64);
 		}
-		genome_size = estimate_genome_size1(signs);	
+		genome_size = estimate_genome_size1(signs);
 		fillusigs(entity, signs, args.bbits);
 	} else if (2 == args.minhashtype) { // multi binvals one hash function
 		const uint64_t nbins = args.sketchsize64 * NBITS(uint64_t);
@@ -163,10 +164,10 @@ void entity_init(Entity &entity, CBuf &cbuf, const std::string &entityname, cons
 	}
 	double entropy = bhmath_calc_entropy(cbuf.chfreqs, 256);
 	entity.matchprob = bhmath_matchprob(args.kmerlen, entropy, genome_size + 1);
-	// fprintf(stderr, "Entropy = %f, genome_size = %lu, matchprob = %f\n", entropy, genome_size + 1, entity.matchprob);	
+	// fprintf(stderr, "Entropy = %f, genome_size = %lu, matchprob = %f\n", entropy, genome_size + 1, entity.matchprob);
 }
 
-void entities_init(std::vector<Entity> &entities, std::string fname, const Sketchargs &args, 
+void entities_init(std::vector<Entity> &entities, std::string fname, const Sketchargs &args,
 		const std::vector<std::pair<size_t, size_t>> &entityid_to_count_vec,
 		const std::vector<std::string> &entityid_to_name) {
 	// std::cerr << "Initiating fname " << fname << std::endl;
@@ -186,11 +187,11 @@ void entities_init(std::vector<Entity> &entities, std::string fname, const Sketc
 }
 
 int cmddist_filter(double &mutdist, double &pvalue,
-		size_t &raw_intersize, size_t &raw_unionsize,		
+		size_t &raw_intersize, size_t &raw_unionsize,
 		const Entity &query, const Entity &target,
 		const Sketchargs &args1, const Distargs &args2,
 		const std::vector<double> &intersize_to_mutdist) {
-	
+
 	size_t intersize;
 	if (args1.minhashtype <= 0) {
 		intersize = calc_intersize0(raw_intersize, raw_unionsize, query, target, args1.sketchsize64);
@@ -203,7 +204,7 @@ int cmddist_filter(double &mutdist, double &pvalue,
 		}
 		raw_intersize = intersize;
 	}
-	
+
 	mutdist = intersize_to_mutdist[intersize];
 	if (mutdist > args2.mthres) { return 1; }
 	if (0 < intersize) {
@@ -219,14 +220,14 @@ int cmddist_filter(double &mutdist, double &pvalue,
 }
 
 size_t cmddist_buffer(char *&buffer, size_t &buffer_size, size_t &buffer_maxsize,
-		const Entity &query, const Entity &target, 
+		const Entity &query, const Entity &target,
 		double mutdist, const double pvalue, size_t raw_intersize, size_t raw_unionsize) {
 	assert (buffer_maxsize >= LINELEN_MAX);
 	if (buffer_size + LINELEN_MAX >= buffer_maxsize) {
 		buffer_maxsize *= 2;
 		buffer = (char*)xrealloc(buffer, buffer_maxsize);
 	}
-	int inc = snprintf(&buffer[buffer_size], LINELEN_MAX, "%s\t%s\t%.4e\t%.4e\t%lu/%lu\n", 
+	int inc = snprintf(&buffer[buffer_size], LINELEN_MAX, "%s\t%s\t%.4e\t%.4e\t%lu/%lu\n",
 			query.name.c_str(), target.name.c_str(), mutdist, pvalue, raw_intersize, raw_unionsize);
 	assert(inc < LINELEN_MAX);
 	assert(inc > 0);
@@ -258,14 +259,14 @@ int cmddist_print(FILE *outfile, const Entity &query, const Entity &target, doub
 const char *ordinal_num_to_suffix(const size_t n) {
 	if (n % 10 > 3 || n % 10 == 0 || (10 <= n % 100 && n % 100 <= 20)) {
 		return "th";
-	}	
+	}
 	if (n % 10 == 1) { return "st"; }
 	if (n % 10 == 2) { return "nd"; }
 	if (n % 10 == 3) { return "rd"; }
 	abort();
 }
 
-inline void printinfo(const size_t i2max, const size_t j2max, const size_t i2gridmax, const size_t j2gridmax, 
+inline void printinfo(const size_t i2max, const size_t j2max, const size_t i2gridmax, const size_t j2gridmax,
 		const clock_t &begclock, const time_t &begtime) {
 	if ((ispowof2(i2max) || i2max == i2gridmax) && (ispowof2(j2max) || j2max == j2gridmax)) {
 		time_t endtime;
@@ -282,9 +283,9 @@ void cmddist(
 #else
 void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 #endif
-		const std::vector<Entity> &entities1, const std::vector<Entity> &entities2, 
+		const std::vector<Entity> &entities1, const std::vector<Entity> &entities2,
 		const Sketchargs &args1, const Distargs &args2) {
-	
+
 	size_t nthreads = args2.nthreads;
 	if (0 == nthreads) {
 #if defined(_OPENMP)
@@ -293,9 +294,9 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 		nthreads = 1;
 #endif
 	}
-	
+
 	FILE *outfile;
-	if ("-" == args2.outfname) { 
+	if ("-" == args2.outfname) {
 		outfile = stdout;
 	} else {
 		outfile = fopen(args2.outfname.c_str(), "w");
@@ -321,7 +322,7 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 	}
 
 	clock_t begclock = clock();
-	time_t begtime;	
+	time_t begtime;
 	time(&begtime);
 
 	if (!tNNEIGHBORS) {
@@ -331,39 +332,39 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 			size_t j2start = (tCLUSTER ? (i2+0) : 0);
 			for (size_t j2 = j2start; j2 < entities2.size(); j2 += CACHE_SIZE) {
 					size_t j2max = MIN(j2 + CACHE_SIZE, entities2.size());
-				
-#if defined(_OPENMP)	
-#pragma omp parallel for schedule(dynamic, 1) num_threads(nthreads)	
+
+#if defined(_OPENMP)
+#pragma omp parallel for schedule(dynamic, 1) num_threads(nthreads)
 #endif
 				for (size_t i = i2; i < i2max; i++) {
 					for (size_t j = (tCLUSTER ? MAX(i+1, j2) : j2); j < j2max; j++) {
 						double mutdist, pvalue;
 						size_t raw_intersize, raw_unionsize;
 						int filter = cmddist_filter(mutdist, pvalue,
-								raw_intersize, raw_unionsize,		
+								raw_intersize, raw_unionsize,
 								entities1[i], entities2[j],
 								args1, args2,
-								intersize_to_mutdist);	
+								intersize_to_mutdist);
 						if (filter) { continue; }
 						size_t iter = i - i2;
 						assert(iter < ISIZE);
 						cmddist_buffer(buffers[iter], buffer_sizes[iter], buffer_maxsizes[iter],
 								entities1[i], entities2[j], mutdist, pvalue, raw_intersize, raw_unionsize);
 					}
-				}			
+				}
 				printinfo(i2max, j2max, entities2.size(), entities2.size(), begclock, begtime);
 				cmddist_write(outfile, buffers, buffer_sizes, i2max - i2);
 			}
 		}
 
 	} else {
-		typedef std::tuple<double, double, size_t, size_t, size_t, size_t> hit_t; 
+		typedef std::tuple<double, double, size_t, size_t, size_t, size_t> hit_t;
 		std::array<std::priority_queue<hit_t>, ISIZE> tophits_arr;
 		for (size_t i2 = 0; i2 < entities1.size(); i2 += ISIZE) {
 			size_t i2max = MIN(i2 + ISIZE, entities1.size());
 			for (size_t j2 = 0; j2 < entities2.size(); j2 += CACHE_SIZE) {
 				size_t j2max = MIN(j2 + CACHE_SIZE, entities2.size());
-				
+
 #if defined(_OPENMP)
 #pragma omp parallel for schedule(dynamic, 1) num_threads(nthreads)
 #endif
@@ -373,10 +374,10 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 						double mutdist, pvalue;
 						size_t raw_intersize, raw_unionsize;
 						int filter = cmddist_filter(mutdist, pvalue,
-								raw_intersize, raw_unionsize,		
+								raw_intersize, raw_unionsize,
 								entities1[i], entities2[j],
 								args1, args2,
-								intersize_to_mutdist);	
+								intersize_to_mutdist);
 						if (filter) { continue; }
 						size_t iter = i - i2;
 						assert(iter < ISIZE);
@@ -390,7 +391,7 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 						}
 					}
 				}
-				printinfo(i2max, j2max, entities1.size(), entities2.size(), begclock, begtime);	
+				printinfo(i2max, j2max, entities1.size(), entities2.size(), begclock, begtime);
 			}
 
 #if defined(_OPENMP)
@@ -404,18 +405,18 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 					tophits.pop();
 					auto mutdist = std::get<0>(hit);
 					auto pvalue = std::get<1>(hit);
-					auto raw_intersize = std::get<2>(hit);	
-					auto raw_unionsize = std::get<3>(hit);	
+					auto raw_intersize = std::get<2>(hit);
+					auto raw_unionsize = std::get<3>(hit);
 					auto qid = std::get<4>(hit);
 					auto tid = std::get<5>(hit);
 					cmddist_buffer(buffers[iter], buffer_sizes[iter], buffer_maxsizes[iter],
 							entities1[qid], entities2[tid], mutdist, pvalue, raw_intersize, raw_unionsize);
 				}
-			}	
+			}
 			cmddist_write(outfile, buffers, buffer_sizes, i2max - i2);
 		}
 	}
-	
+
 	for (size_t i = 0; i < ISIZE; i++) {
 		free(buffers[i]);
 	}
@@ -425,14 +426,14 @@ void cmddist(bool tCLUSTER, bool tNNEIGHBORS,
 int main(int argc, char **argv) {
 	time_t begtime, endtime;
 	time(&begtime);
-	
+
 	std::string commitsuffix = "";
 	if (std::string("()") == (STR((GIT_DIFF_SHORTSTAT)))) {
 		commitsuffix = "-clean";
 	} else {
 		commitsuffix = std::string("-dirty ") + (STR((GIT_DIFF_SHORTSTAT)));
 	}
-	
+
 	if (argc < 2 || !strcmp("--help", argv[1])) { allusage(argc, argv); }
 	if (!strcmp("--version", argv[1])) {
 		std::cerr << "version " << VERSION << " commit " << (STR(GIT_COMMIT_HASH)) << commitsuffix << std::endl;
@@ -450,13 +451,13 @@ int main(int argc, char **argv) {
 		}
 		for (size_t i = 0; i < kmersetVec.size(); i++) {
 			for (size_t j = i + 1; j < kmersetVec.size(); j++) {
-				kmerset_dist(kmersetVec[i], kmersetVec[j], args.kmerlen);			
+				kmerset_dist(kmersetVec[i], kmersetVec[j], args.kmerlen);
 			}
 		}
 	} else if (!strcmp("sketch", argv[1])) {
 		Sketchargs args;
-		args.parse(argc, argv);	
-		
+		args.parse(argc, argv);
+
 		std::vector<std::vector<std::pair<size_t, size_t>>> fid_to_entityid_count_list;
 		std::vector<std::string> fid_to_fname;
 		std::vector<std::string> entityid_to_entityname;
@@ -465,8 +466,8 @@ int main(int argc, char **argv) {
 		parse_metaf(fid_to_entityid_count_list, fid_to_fname, entityid_to_entityname, args.infnames);
 		assert (fid_to_entityid_count_list.size() == fid_to_fname.size());
 		std::vector<Entity> entities(entityid_to_entityname.size(), Entity());
-		
-		
+
+
 		clock_t para_begclock = clock();
 		time_t para_begtime, para_endtime;
 		time(&para_begtime);
@@ -478,8 +479,8 @@ int main(int argc, char **argv) {
 			// entity_init(entities[i], args.infnames[i], args);
 			if (0 == (i & (i + 1))) {
 				time(&para_endtime);
-				std::cerr << "Sketching the first " <<  i + 1 << " files consumed " 
-				          << (clock() - para_begclock) / CLOCKS_PER_SEC << " CPU seconds and " 
+				std::cerr << "Sketching the first " <<  i + 1 << " files consumed "
+				          << (clock() - para_begclock) / CLOCKS_PER_SEC << " CPU seconds and "
 				          << difftime(para_endtime, para_begtime) << " real seconds" << std::endl;
 				          // << "The last sequence is converted into " << entities[i].usigs.size() << " 64-bit integers.\n";
 			}
@@ -487,17 +488,17 @@ int main(int argc, char **argv) {
 		save_entities(entities, args.outfname);
 		time(&endtime);
 		std::string systime_began2 = ctime(&begtime);
-		std::string systime_ended2 = ctime(&endtime);	
+		std::string systime_ended2 = ctime(&endtime);
 		args.write(systime_began2, systime_ended2, clock() / CLOCKS_PER_SEC);
 	} else if (!strcmp("dist", argv[1])) {
 		Distargs args;
 		args.parse(argc, argv);
-	
+
 		Sketchargs args1;
 		std::vector<Entity> entities1;
 		args1.read(args.infnames[0]); // args1.usage(argc, argv);
 		load_entities(entities1, args.infnames[0]);
-	
+
 		std::vector<Entity> entities2;
 		if (args.infnames.size() == 2) {
 			Sketchargs args2;
@@ -510,7 +511,7 @@ int main(int argc, char **argv) {
 		}
 
 		const bool tCLUSTER = (1 == args.infnames.size());
-		const bool tNNEIGHBORS = (0 < args.nneighbors);	
+		const bool tNNEIGHBORS = (0 < args.nneighbors);
 #if DIST_IS_TEMPLATED
 		if (tCLUSTER && tNNEIGHBORS) {
 			cmddist<true, true>(entities1, entities1, args1, args);
@@ -528,7 +529,7 @@ int main(int argc, char **argv) {
 		std::cerr << "Unrecognized command: " << argv[1] <<  "\n";
 		allusage(argc, argv);
 	}
-	
+
 	time(&endtime);
 	std::cerr << "Program ran for " << clock() / CLOCKS_PER_SEC << " CPU seconds and " << difftime(endtime, begtime) << " real seconds." << std::endl;
 }
